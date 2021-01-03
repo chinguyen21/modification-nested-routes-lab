4b0fb54b2706eda85872c86bb96351157e10087e
Chi Nguyen <nguyenchiftu2101@gmail.com>
2021-01-02 19:59:43 -0500

    Done.

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -1,5 +1,13 @@
 module ArtistsHelper
+
+
   def display_artist(song)
     song.artist.nil? ? link_to("Add Artist", edit_song_path(song)) : link_to(song.artist_name, artist_path(song.artist))
   end
+
+
+    def edit_artist(song)
+      params[:artist_id].nil? ? link_to("Edit Song", edit_song_path(song)) : link_to("Edit Song", edit_artist_song_path(song.artist, song))
+    end
+
 end
